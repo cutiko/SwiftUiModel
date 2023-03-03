0cac6c10b63f5330040a038ed244fda2525480c8
cutiko <erick.navarro@vacasa.com>
2023-03-03 11:21:16 -0300

    Initial Commit

---
+++ UiModel/UiModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiModelApp.swift
+//  UiModel
+//
+//  Created by Erick Navarro on 03-03-23.
+//
+
+import SwiftUI
+
+@main
+struct UiModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
